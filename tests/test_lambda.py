1fbfcf89097255bf2ec874029ad821bc9db8a381
Aleksandr Shevchenko <a.shevchenko@sigma.ru>
2023-09-30 15:23:04 +0300

    homework for 6.9

---
+++ tests/test_lambda.py
@@ -0,0 +1,33 @@
+import allure
+from allure_commons.types import Severity
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+
+@allure.tag('Normal')
+@allure.severity(Severity.NORMAL)
+@allure.label('owner', 'Filos0v')
+@allure.story('Test Selene Lambda')
+@allure.link("https://github.com", name="Testing")
+def test_lambda_steps():
+    with allure.step(('Открываем главную страницу')):
+        browser.open('https://github.com')
+
+    with allure.step(('Ищем репозиторий')):
+        s('.header-search-button').click()
+        s('#query-builder-test').send_keys('eroshenkoam/allure-example')
+        s('#query-builder-test').submit()
+
+    with allure.step(('Переходим по ссылке репозитория')):
+        s(by.link_text('eroshenkoam/allure-example')).click()
+
+    with allure.step(('Открываем таб Issue')):
+        s('#issues-tab').click()
+
+    with allure.step(('Ищем номер 77')):
+        s(by.partial_text('#77')).should(be.visible)
+
+    with allure.step("Закрываем браузер"):
+        browser.quit()
---
+++ tests/test_selene.py
@@ -0,0 +1,25 @@
+import allure
+from allure_commons.types import Severity
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+
+@allure.story('selene')
+@allure.label('owner', 'Filos0v')
+@allure.tag('web')
+@allure.severity(Severity.NORMAL)
+def test_github():
+    browser.open('https://github.com')
+
+    browser.element(".header-search-button").click()
+    browser.element("#query-builder-test").send_keys("eroshenkoam/allure-example")
+    browser.element("#query-builder-test").submit()
+
+    browser.element(by.link_text('eroshenkoam/allure-example')).click()
+
+    browser.element('#issues-tab').click()
+
+    browser.element(by.partial_text('#76')).should(be.visible)
+
