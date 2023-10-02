e0f27fa4a62a34680fc02d260dfe12e1cd33a9f8
Aleksandr Shevchenko <a.shevchenko@sigma.ru>
2023-10-02 15:23:01 +0300

    fix comments

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -12,19 +12,19 @@ from selene.support.shared.jquery_style import s
 @allure.story('Test Selene Lambda')
 @allure.link("https://github.com", name="Testing")
 def test_lambda_steps():
-    with allure.step(('Открываем главную страницу')):
+    with allure.step('Открываем главную страницу'):
         browser.open('https://github.com')
 
-    with allure.step(('Ищем репозиторий')):
+    with allure.step('Ищем репозиторий'):
         s('.header-search-button').click()
         s('#query-builder-test').send_keys('eroshenkoam/allure-example')
         s('#query-builder-test').submit()
 
-    with allure.step(('Переходим по ссылке репозитория')):
+    with allure.step('Переходим по ссылке репозитория'):
         s(by.link_text('eroshenkoam/allure-example')).click()
 
-    with allure.step(('Открываем таб Issue')):
+    with allure.step('Открываем таб Issue'):
         s('#issues-tab').click()
 
-    with allure.step(('Ищем номер 77')):
+    with allure.step('Ищем номер 77'):
         s(by.partial_text('#77')).should(be.visible)
--- tests/test_selene.py
+++ tests/test_selene.py
@@ -3,7 +3,6 @@ from allure_commons.types import Severity
 from selene.support import by
 from selene.support.conditions import be
 from selene.support.shared import browser
-from selene.support.shared.jquery_style import s
 
 
 @allure.story('selene')
